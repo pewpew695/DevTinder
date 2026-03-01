81ff74458f6595912c6d9ce91cc4e44217f3371e
pewpew695 <ashpd02@gmail.com>
2026-03-01 17:34:26 +0530

    Check Different options related to routing in express.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
+// app.use("/", (req, res) => {
+//   res.send("Namaste!!");
+// });
+
+app.get("/user/:userID", (req, res) => {
+  console.log(req.params);
+  res.send("This is user");
+});
+
+app.get("/test", (req, res) => {
   res.send("This is the server!!!!!");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello hello");
 });
 
 app.listen(3000, () => {
